aa935cb406445a91ec518327c1b9300498536722
Rafael Aguilar Martín <rais38@gmail.com>
2013-03-03 15:15:59 +0100

    Added UIView Category

---
+++ DemoSwizzling/DemoSwizzling/UIView+BorderColor.m
@@ -0,0 +1,13 @@
+//
+//  UIView+BorderColor.m
+//  DemoSwizzling
+//
+//  Created by Rafael Aguilar Martín on 03/03/13.
+//  Copyright (c) 2013 Rafael Aguilar Martín. All rights reserved.
+//
+
+#import "UIView+BorderColor.h"
+
+@implementation UIView (BorderColor)
+
+@end
